7a331666712969d486fac9fe52e97d9d96df939e
akshad07 <akshadpardeshi07@gmail.com>
2024-06-26 18:05:12 +0530

    url

--- restaurants_project/restaurants/urls.py
+++ restaurants_project/restaurants/urls.py
@@ -3,15 +3,8 @@ from django.urls import path
 from .views import RestaurantsListview, RestaurantCreateView, RestaurantMapView
 app_name = "restaurants"
 
-urlpatterns = [
-    # path(
-    #     "map/", MarkersMapView.as_view(),
-    #     name="map"
-    # ),
-    path(
-        "", RestaurantsListview.as_view(),
-        name="restaurants_list"
-    ),
+urlpatterns = [    
+    path("", RestaurantsListview.as_view(),name="restaurants_list"),
     path('create/', RestaurantCreateView.as_view(), name='create_restaurant'),
     path('map/', RestaurantMapView.as_view(), name='map'),
 ]
