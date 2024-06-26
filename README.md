5796ad0f144f7d81455213a19bce54a670374ef1
akshad07 <akshadpardeshi07@gmail.com>
2024-06-26 17:41:47 +0530

    Read me File

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# restaurants
+
+"Register and view restaurants on a map. This Django-based web application allows users to register new restaurants, specifying their names, opening times, and locations via latitude and longitude. The map interface visually displays all registered restaurants, providing a convenient overview of their geographical distribution."
+
+## Setup Instructions
+
+1. Clone the repository:
+    ```sh
+    git clone <repository-url>
+    cd csv_analysis_project
+    ```
+
+2. Create and activate a virtual environment:
+    ```sh
+    python -m venv env
+    source env/bin/activate  # On Windows use `env\Scripts\activate`
+    ```
+
+3. Install the required packages:
+    ```sh
+    pip install -r requirements.txt
+    ```
+
+4. Apply the migrations:
+    ```sh
+    python restaurants_project/manage.py makemigrations
+    python restaurants_project/manage.py migrate
+    ```
+
+5. Start the development server:
+    ```sh
+    python restaurants_project/manage.py runserver
+    ```
+
+6. Open a web browser and navigate to `http://127.0.0.1:8000/' to Register and View Restaurants
+
+Admin Panel:
+    Username = 'admin'
+    password = 'admin'
+
+Or You Can use Docker also
+
+    Docker Setup
+    Alternatively, you can use Docker for simplified setup:
+
+    1.Build and run the Docker containers:
+    
+        docker-compose up --build   #Run this command 
+
+    2.Open a web browser and navigate to `http://127.0.0.1:8000/' to Register and View Restaurants
+
--- restaurants_project/restaurants/views.py
+++ restaurants_project/restaurants/views.py
@@ -1,24 +1,35 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from .models import Restaurant
 from django.views.generic import ListView, CreateView
+from .models import Restaurant
 from django.contrib.gis.geos import GEOSGeometry
 from django.core.serializers import serialize
 from django.urls import reverse_lazy
 
 
 class RestaurantsListview(ListView):
+    """
+    View for listing all restaurants.
+    Uses ListView to display a list of restaurants.
+    """
     model = Restaurant
     template_name = "dashboard/restaurants_list.html"
     context_object_name = "restaurants"
 
 
 class RestaurantCreateView(CreateView):
+    """
+    View for creating a new restaurant.
+    Uses CreateView to handle form submission and create a new restaurant instance.
+    """
     model = Restaurant
     template_name = 'dashboard/restaurant_add.html'
     fields = ['name', 'opentime', 'address']  # Exclude location field as it will be handled separately
 
     def form_valid(self, form):
+        """
+        Override form_valid to handle location data separately and set it in the form instance.
+        """
         latitude = self.request.POST.get('latitude')
         longitude = self.request.POST.get('longitude')
         location = f"POINT({longitude} {latitude})"
@@ -29,24 +40,42 @@ class RestaurantCreateView(CreateView):
         return super().form_valid(form)
 
     def get_success_url(self):
+        """
+        Redirect to the restaurants list view after successful form submission.
+        """
         return reverse_lazy('restaurants:restaurants_list')
 
 
-
 class RestaurantMapView(TemplateView):
+    """
+    View for displaying a map with restaurant locations.
+    Uses TemplateView to render a template containing a map.
+    """
     template_name = "dashboard/restaurant_map.html"
 
     def get_context_data(self, **kwargs):
+        """
+        Override get_context_data to provide additional context data for the template.
+        Serializes restaurant locations and prepares restaurant data for the map.
+        """
         context = super().get_context_data(**kwargs)
-        # Serialize Restaurant locations
+        
+        # Serialize Restaurant locations as GeoJSON
         context['restaurants'] = serialize("geojson", Restaurant.objects.all(),
                                            geometry_field="location", fields=["name", "opentime", "address"])
+        
+        # Prepare restaurant data for map markers
         restaurant_data = []
         for restaurant in Restaurant.objects.all():
             point = GEOSGeometry(restaurant.location.wkb)
             lat, lon = point.y, point.x
-            restaurant_data.append({'lat': lat, 'lon': lon, 'name': restaurant.name,
-                                    'opentime': restaurant.opentime.strftime('%H:%M:%S'),
-                                    'address': restaurant.address})
+            restaurant_data.append({
+                'lat': lat,
+                'lon': lon,
+                'name': restaurant.name,
+                'opentime': restaurant.opentime.strftime('%H:%M:%S'),
+                'address': restaurant.address
+            })
+        
         context['restaurant_data'] = restaurant_data
         return context
